108af29fcd042ba3bd5f9c57046e115bae8e025b
Mustafa <mustafa.93717@gmail.com>
2026-08-17 23:41:10 +0530

    Add C++ solution for Repetitions

--- Introductory_Problems/Repetitions/Repetitions.cpp
+++ Introductory_Problems/Repetitions/Repetitions.cpp
@@ -7,5 +7,5 @@
 
 input/code.cpp: In function 'int main()':
 input/code.cpp:7:18: warning: comparison of integer expressions of different signedness: 'int' and 'std::__cxx11::basic_string<char>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
-    7 |     for(int i=0;i<s.size();i++){
-      |                 ~^~~~~~~~~
+    7 |     for(int i=0;i<s.size()-1;i++){
+      |                 ~^~~~~~~~~~~

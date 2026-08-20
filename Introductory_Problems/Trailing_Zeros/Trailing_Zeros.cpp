97b6d9937f50be51e84740d4a486e87223346fba
Mustafa <mustafa.93717@gmail.com>
2026-08-21 00:25:44 +0530

    Add C++ solution for Trailing_Zeros

---
+++ Introductory_Problems/Trailing_Zeros/Trailing_Zeros.cpp
@@ -0,0 +1,14 @@
+/*
+ * Problem Name: Trailing_Zeros
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-08-20
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    cout<<n/5<<endl;
+}

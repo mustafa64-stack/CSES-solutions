990fd681a63a7c36b3cf82582b9d9c1e1ceb39a6
Mustafa <mustafa.93717@gmail.com>
2026-08-23 10:59:49 +0530

    Add C++ solution for Trailing_Zeros

--- Introductory_Problems/Trailing_Zeros/Trailing_Zeros.cpp
+++ Introductory_Problems/Trailing_Zeros/Trailing_Zeros.cpp
@@ -2,13 +2,15 @@
  * Problem Name: Trailing_Zeros
  * Language: C++
  * Category: Introductory_Problems
- * Date: 2026-08-20
+ * Date: 2026-08-23
  */
 
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    long long n;
+    long long n,ans=0;
     cin>>n;
-    cout<<n/5<<endl;
+    for(long long i=5;i<=n;i*=5){
+        ans+=(n/i);
+    }cout<<ans<<endl;
 }

0503ee067ac17d737a4787996dbbe030fa56d6f6
Mustafa <mustafa.93717@gmail.com>
2026-08-23 15:15:02 +0530

    Add C++ solution for Palindrome_Reorder

---
+++ Introductory_Problems/Palindrome_Reorder/Palindrome_Reorder.cpp
@@ -0,0 +1,14 @@
+/*
+ * Problem Name: Palindrome_Reorder
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-08-23
+ */
+
+input/code.cpp: In function 'int main()':
+input/code.cpp:18:14: warning: unused variable 'ch' [-Wunused-variable]
+   18 |         char ch;
+      |              ^~
+input/code.cpp:19:13: warning: unused variable 'cnt' [-Wunused-variable]
+   19 |         int cnt;
+      |             ^~~

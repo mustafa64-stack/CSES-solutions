3f5bebdafe46356266e7772ec3d95fc69b59ff4b
Mustafa <mustafa.93717@gmail.com>
2026-08-25 19:18:55 +0530

    Add C++ solution for Tower_of_Hanoi

---
+++ Introductory_Problems/Tower_of_Hanoi/Tower_of_Hanoi.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem Name: Tower_of_Hanoi
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-08-25
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    cout<<2*n-1<<"\n";
+    for(int i=1;i<=n-1;i++){
+        cout<<"1 2\n";
+    }cout<<"1 3\n";
+    for(int i=1;i<=n-1;i++){
+        cout<<"1 2\n";
+    }
+}

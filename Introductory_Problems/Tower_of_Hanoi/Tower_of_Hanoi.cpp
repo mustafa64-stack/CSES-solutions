c6ed861b332218e20de0b98bb40bb8b011ba3e07
Mustafa <mustafa.93717@gmail.com>
2026-08-26 18:35:21 +0530

    Add C++ solution for Tower_of_Hanoi

--- Introductory_Problems/Tower_of_Hanoi/Tower_of_Hanoi.cpp
+++ Introductory_Problems/Tower_of_Hanoi/Tower_of_Hanoi.cpp
@@ -2,19 +2,23 @@
  * Problem Name: Tower_of_Hanoi
  * Language: C++
  * Category: Introductory_Problems
- * Date: 2026-08-25
+ * Date: 2026-08-26
  */
 
 #include <bits/stdc++.h>
 using namespace std;
+void solve(int discs,int from,int to,int aux){
+    if(discs==1) {cout<<from<<" "<<to<<endl;
+        return ;}
+ 
+    solve(discs-1,from,aux,to);
+    cout<<from<<" "<<to<<endl;;
+    solve(discs-1,aux,to,from);
+    
+}
 int main(){
     int n;
     cin>>n;
-    cout<<2*n-1<<"\n";
-    for(int i=1;i<=n-1;i++){
-        cout<<"1 2\n";
-    }cout<<"1 3\n";
-    for(int i=1;i<=n-1;i++){
-        cout<<"2 3\n";
-    }
+    cout<<(1<<n)-1<<"\n";
+    solve(n,1,3,2);
 }

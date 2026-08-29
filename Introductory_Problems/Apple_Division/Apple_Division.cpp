7eeb30c9ec3c7550e7b284ee1419048cf8078c2a
Mustafa <mustafa.93717@gmail.com>
2026-08-29 12:43:16 +0530

    Add C++ solution for Apple_Division

---
+++ Introductory_Problems/Apple_Division/Apple_Division.cpp
@@ -0,0 +1,11 @@
+/*
+ * Problem Name: Apple_Division
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-08-29
+ */
+
+input/code.cpp: In function 'void solve(std::vector<long long int>, long long int, long long int, int)':
+input/code.cpp:5:9: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<long long int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+    5 |     if(i==p.size())
+      |        ~^~~~~~~~~~
